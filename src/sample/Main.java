be14d85e00f9a5924f7c9a9a3f7c6885a496d06d
kchan2203 <62119221+kchan2203@users.noreply.github.com>
2020-03-12 14:13:50 -0700

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-		System.out.println("This is a test");
+	System.out.println("This is a test");
     }
 
 
