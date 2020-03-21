65225d59c070587302a1ca5c0542ad08d6933768
kchan2203 <kevinchan03@gmail.com>
2020-03-21 12:32:00 -0700

    added MainGame functionality added Shop class added GoldDisplay class added StorageDeck class added Playfield class added Score class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,14 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        MainGame root2 = new MainGame();
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root2.getWrapper(), 1000, 1000));
         primaryStage.show();
 
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args); }
 }
