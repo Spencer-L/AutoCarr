bb1d3cc9b849869292059307e5c7615ddf6ace24
Spencer-L <31945337+Spencer-L@users.noreply.github.com>
2020-05-24 16:58:32 -0700

    Add files via upload

---
+++ RectPiece.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+//paladin class
+public class RectPiece extends Piece{
+    private Rectangle base;
+    RectPiece(double s,double h, String id,MainGame mG,PlayField pF,int tN,double[] pos,Deck pD){
+        super(s,h,id,mG,pF,tN,pos,pD);
+        setHealth(getHealth()+200);
+        setMaxHealth((int)getHealth());
+        setAtkSpd(.9);
+        setDamage(20);
+        setRange(1);
+        base=new Rectangle(0,0,getSize(),getSize());
+        if(getTeamNum()==1){
+            base.setFill(Color.VIOLET);
+        }else if(getTeamNum()==2){
+            base.setFill(Color.BROWN);
+        }
+
+        getBody().getChildren().add(base);
+        getBody().setAlignment(getRarityBand(), Pos.TOP_CENTER);
+        getBody().getChildren().add(getRarityBand());
+        getBody().getChildren().add((getLevels()));
+        getBody().setAlignment(getHealthBarRed(), Pos.BOTTOM_CENTER);
+        getBody().getChildren().add(getHealthBarRed());
+
+    }
+}
